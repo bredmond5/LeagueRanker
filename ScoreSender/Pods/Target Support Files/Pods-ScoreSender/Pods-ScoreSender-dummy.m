0259ed5fb722950f2a2fdc45346e8cb217bdeb98
Brice Redmond <briceredmond@gmail.com>
2020-04-08 21:54:18 -0700

    added firebase

---
+++ ScoreSender/Pods/Target Support Files/Pods-ScoreSender/Pods-ScoreSender-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScoreSender : NSObject
+@end
+@implementation PodsDummy_Pods_ScoreSender
+@end
---
+++ ScoreSender/Pods/Target Support Files/Pods-ScoreSenderTests/Pods-ScoreSenderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScoreSenderTests : NSObject
+@end
+@implementation PodsDummy_Pods_ScoreSenderTests
+@end
